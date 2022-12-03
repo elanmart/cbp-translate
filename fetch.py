8d502ea6584f2719ae2ab7de375934a9dd1670ec
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-12-03 19:40:51 +0100

    Add a script to fetch the YT clips

---
+++ fetch.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+
+import yaml
+import yt_dlp
+
+
+def download(url: str, path_out: Path):
+
+    audio = {"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}
+    kwd = dict(
+        outtmpl=str(path_out), quiet=True, keepvideo=True, postprocessors=[audio]
+    )
+
+    with yt_dlp.YoutubeDL(kwd) as ydl:
+        ydl.download([url])
+
+
+def downlad_all(inputs: str, output: str):
+    with open(inputs, "r") as f:
+        clips = yaml.load(f, yaml.SafeLoader)
+
+    for name, url in clips.items():
+        path_out = Path(output) / f"{name}.%(ext)s"
+        download(url=url, path_out=path_out)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputs", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+    args = parser.parse_args()
+    downlad_all(**args.__dict__)
