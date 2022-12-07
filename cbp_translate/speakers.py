21b762736dbfbd0bc89f18d4ecb18f0c768bba5d
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-12-07 23:21:15 +0100

    feat: add speaker diarization code

---
+++ cbp_translate/speakers.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from huggingface_hub import login  # type: ignore
+from pyannote.audio import Pipeline
+from pyannote.core import Annotation
+
+
+@dataclass
+class SpeakerSegment:
+    id_: str
+    start: float
+    end: float
+
+
+def hf_login():
+    token = Path("~/.huggingface/token").expanduser().read_text().strip()
+    login(token=token)
+
+
+def extract_speakers(path_audio: str):
+    hf_login()
+
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization@develop", use_auth_token=True  # type: ignore
+    )
+    dia = pipeline(path_audio)
+
+    ret = []
+    for speech_turn, _, speaker in dia.itertracks(yield_label=True):
+        ret.append(SpeakerSegment(speaker, speech_turn.start, speech_turn.end))
+
+    return ret
