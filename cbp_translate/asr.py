dc0caaf86b90fdcb15dc4a0c65f07e0a550442b4
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-12-07 23:40:11 +0100

    chore: move ASR to its own file

---
+++ cbp_translate/asr.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import cast
+
+import whisper
+
+
+@dataclass
+class SpeechSegment:
+    start: float
+    end: float
+    text_src: str
+    text_tgt: str = ""
+
+
+def extract_segments(path: str) -> list[SpeechSegment]:
+    model = whisper.load_model("large", "cuda")
+    result = whisper.transcribe(model, path)
+
+    segments = []
+    for s in result["segments"]:
+        s = cast(dict, s)
+        segments.append(SpeechSegment(s["start"], s["end"], s["text"]))
+
+    return segments
