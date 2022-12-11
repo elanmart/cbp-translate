10ac69eaba60f6ac9c7bf9086f78b1cb6bfb5bbd
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-12-11 20:00:34 +0100

    feat: re-write face detection to use agglomerative clustering

--- cbp_translate/faces.py
+++ cbp_translate/faces.py
@@ -1,26 +1,20 @@
-from dataclasses import dataclass
-from typing import NamedTuple
+from typing import Iterable, Iterator, NamedTuple
 
 import cv2
 import numpy as np
 from deepface import DeepFace
-from deepface.commons import distance as adst
 from deepface.commons import functions
-from numba import cuda
 from retinaface import RetinaFace
 from retinaface.commons import postprocess
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import pairwise_distances
 from tensorflow.keras.preprocessing import image  # type: ignore
 
 from . import Arr
 
 FaceId = int
-
-
-@dataclass
-class FaceEmbedding:
-    id_: "FaceId"
-    embedding: Arr
-    frequency: int = 1
+Image = np.ndarray
+Embedding = np.ndarray
 
 
 class FaceLocation(NamedTuple):
@@ -30,13 +24,26 @@ class FaceLocation(NamedTuple):
     y1: int
 
 
-class Face(NamedTuple):
-    id_: FaceId
+class DetectedFace(NamedTuple):
     location: FaceLocation
+    image: Image = np.array([])
+    embedding: Embedding = np.array([])
+    id_: FaceId = -1
+
+    @property
+    def area(self):
+        return (self.location.x1 - self.location.x0) * (
+            self.location.y1 - self.location.y0
+        )
+
+    @property
+    def height_ratio(self):
+        return (self.location.y1 - self.location.y0) / (
+            self.location.x1 - self.location.x0
+        )
 
 
-OnFrameFaces = list[Face]
-FaceDB = dict[FaceId, FaceEmbedding]
+OnFrameFaces = list[DetectedFace]
 
 
 def _process_face(img: Arr, target_size: tuple[int, int]):
@@ -84,67 +91,68 @@ def _process_face(img: Arr, target_size: tuple[int, int]):
     return img_pixels
 
 
-def _extract_faces(
-    img_path, threshold=0.9, model=None, align=True, allow_upscaling=True
-):
+def _detect_faces(
+    img_path, threshold=0.95, model=None, align=True, allow_upscaling=True
+) -> OnFrameFaces:
     """Copied from retinaface.RetinaFace to return both the image and the facial area.
 
     Credits: https://github.com/serengil/retinaface
     """
 
-    resp = []
+    faces: OnFrameFaces = []
+
     img = RetinaFace.get_image(img_path)
     obj = RetinaFace.detect_faces(
         img_path=img, threshold=threshold, model=model, allow_upscaling=allow_upscaling
     )
 
     if type(obj) == dict:
-        for key in obj:
+        for key in obj:  # type: ignore
             identity = obj[key]  # type: ignore
 
-            facial_area = identity["facial_area"]
+            facial_area = identity["facial_area"]  # type: ignore
             facial_img = img[
                 facial_area[1] : facial_area[3], facial_area[0] : facial_area[2]
             ]  # type: ignore
 
             if align == True:
-                landmarks = identity["landmarks"]
+                landmarks = identity["landmarks"]  # type: ignore
                 left_eye = landmarks["left_eye"]
                 right_eye = landmarks["right_eye"]
                 nose = landmarks["nose"]
-                mouth_right = landmarks["mouth_right"]
-                mouth_left = landmarks["mouth_left"]
 
-                facial_img = postprocess.alignment_procedure(
-                    facial_img, right_eye, left_eye, nose
-                )
+                try:
+                    # TODO: investigate why retinaface is failing here for some frames
+                    facial_img = postprocess.alignment_procedure(
+                        facial_img, right_eye, left_eye, nose
+                    )
+                except ValueError as e:
+                    continue
 
-            facial_img = facial_img[:, :, ::-1]  # type: ignore
-            resp.append((facial_img, facial_area))
+            img = facial_img[:, :, ::-1]  # type: ignore
+            loc = FaceLocation(*facial_area)
+            face = DetectedFace(loc, image=img)
 
-    return resp
+            faces.append(face)
 
-
-def add(database: dict[FaceId, FaceEmbedding], embedding: Arr):
-
-    for id_, face in database.items():
-        dist = adst.findCosineDistance(embedding, face.embedding)
-        if dist < 0.68:
-            face.frequency += 1
-            face.embedding = (
-                face.embedding * (face.frequency - 1) + embedding
-            ) / face.frequency
-            return id_
-    else:
-        id_ = len(database)
-        database[id_] = FaceEmbedding(id_=id_, embedding=embedding)
-        return id_
+    return faces
 
 
-def assign_and_localize_faces(frames: list[Arr]) -> tuple[FaceDB, list[OnFrameFaces]]:
+def _filter_faces(
+    faces: OnFrameFaces, top_k: int = 3, ratio: float = 3.0
+) -> OnFrameFaces:
+    faces = [f for f in faces if f.height_ratio < ratio]
+    faces = sorted(faces, key=lambda f: f.area, reverse=True)
+    faces = faces[:top_k]
 
-    database: FaceDB = {}
-    faces: list[OnFrameFaces] = []
+    return faces
+
+
+def get_face_embeddings(
+    frames: Iterable[Arr],
+    top_k: int = 3,
+    ratio: float = 3.0,
+) -> Iterator[OnFrameFaces]:
 
     model = DeepFace.build_model("ArcFace")
     retina = RetinaFace.build_model()
@@ -152,44 +160,66 @@ def assign_and_localize_faces(frames: list[Arr]) -> tuple[FaceDB, list[OnFrameFa
 
     for frame in frames:
 
-        detected = _extract_faces(frame[..., ::-1], align=True, model=retina)
-        on_frame = []
+        embedded = []
+        detected = _detect_faces(frame[..., ::-1], align=True, model=retina)
+        detected = _filter_faces(detected, top_k=top_k, ratio=ratio)
 
-        for face_img, face_loc in detected:
+        for face in detected:
 
-            face_loc = FaceLocation(*face_loc)
-
-            face_img = _process_face(face_img, target_size)
+            face_img = _process_face(face.image, target_size)
             face_img = functions.normalize_input(face_img, normalization="base")
-
             embedding = model.predict(face_img)[0]
-            face_id = add(database, embedding)
 
-            on_frame.append(Face(face_id, face_loc))
+            embedded.append(DetectedFace(face.location, embedding=embedding))
 
-        faces.append(on_frame)
-
-    del model, retina
-    device = cuda.get_current_device()
-    device.reset()
-
-    return database, faces
+        yield embedded
 
 
-def postprocess_faces(
-    database: FaceDB, faces: list[OnFrameFaces]
-) -> tuple[FaceDB, list[OnFrameFaces]]:
+def face_clustering(faces: Iterable[OnFrameFaces]) -> Arr:
 
-    database = {key: face for key, face in database.items() if face.frequency > 24}
-    faces = [
-        [face for face in frame_faces if face.id_ in database] for frame_faces in faces
-    ]
+    embeddings = [item.embedding for frame_faces in faces for item in frame_faces]
+    embeddings = np.stack(embeddings)
 
-    return database, faces
+    agc = AgglomerativeClustering(
+        n_clusters=None, affinity="cosine", linkage="average", distance_threshold=0.68  # type: ignore
+    )
+    preds = agc.fit_predict(embeddings)
+    inds, counts = np.unique(preds, return_counts=True)
+    inds = inds[counts > 48]
+
+    centers = np.empty((len(inds), embeddings.shape[1]))
+    for i in range(agc.n_clusters_):
+        index = inds[i]
+        centers[i] = embeddings[preds == index].mean(axis=0)
+
+    return centers
 
 
-def extract_faces(frames: list[Arr]) -> tuple[FaceDB, list[OnFrameFaces]]:
-    database, faces = assign_and_localize_faces(frames)
-    database, faces = postprocess_faces(database, faces)
+def assign_face_ids(
+    faces: Iterable[OnFrameFaces], cluster_centers: Arr
+) -> Iterator[OnFrameFaces]:
 
-    return database, faces
+    for frame_faces in faces:
+
+        frame_faces = sorted(frame_faces, key=lambda f: f.area, reverse=True)
+        embeddings = np.stack([face.embedding for face in frame_faces])
+        distances = pairwise_distances(embeddings, cluster_centers, metric="cosine")
+        annotated = []
+
+        for idx, face in enumerate(frame_faces):
+
+            cluster_idx = np.argmin(distances[idx, :])
+            distances[:, cluster_idx] = np.inf
+            annotated.append(DetectedFace(face.location, id_=int(cluster_idx)))
+
+        yield annotated
+
+
+def extract_faces(frames: Iterable[Arr]) -> Iterator[OnFrameFaces]:
+
+    embeddings = get_face_embeddings(frames)
+    embeddings = list(embeddings)
+    cluster_centers = face_clustering(embeddings)
+    faces = assign_face_ids(embeddings, cluster_centers)
+
+    yield from faces
