5f4c4370eea588dfd2bdd9ce8834582637846f2d
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-12-16 01:08:46 +0100

    feat: adapt speaker diarization to use modal

--- cbp_translate/speakers.py
+++ cbp_translate/speakers.py
@@ -1,8 +1,7 @@
+import os
 from dataclasses import dataclass
-from pathlib import Path
 
-from huggingface_hub import login  # type: ignore
-from pyannote.audio import Pipeline
+from .modal_ import ROOT, gpu_image, hf_secret, stub, volume
 
 
 @dataclass
@@ -12,20 +11,28 @@ class SpeakerSegment:
     end: float
 
 
-def hf_login():
-    token = Path("~/.huggingface/token").expanduser().read_text().strip()
-    login(token=token)
-
-
+@stub.function(
+    image=gpu_image,
+    gpu=True,
+    shared_volumes={str(ROOT): volume},
+    secret=hf_secret, 
+    timeout=30 * 60,
+)
 def extract_speakers(path_audio: str) -> list[SpeakerSegment]:
-    hf_login()
 
+    from pyannote.audio import Pipeline
+
+    (cache_dir := (ROOT / ".hf")).mkdir(exist_ok=True)
+    auth_token = os.environ["HUGGINGFACE_TOKEN"]
     pipeline = Pipeline.from_pretrained(
-        "pyannote/speaker-diarization@2.1.1", use_auth_token=True  # type: ignore
+        "pyannote/speaker-diarization@2.1.1",
+        use_auth_token=auth_token,
+        cache_dir=cache_dir,
     )
-    dia = pipeline(path_audio)
 
+    dia = pipeline(path_audio)
     ret = []
+
     for speech_turn, _, speaker in dia.itertracks(yield_label=True):
         ret.append(SpeakerSegment(speaker, speech_turn.start, speech_turn.end))
 
